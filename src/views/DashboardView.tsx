ae5026aef645564d3a56bd7ce2013814c5415299
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-20 22:07:27 -0400

    mostrando menu de hamburguesa

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,3 +1,8 @@
 export default function DashboardView() {
-  return <div>DashboardView</div>;
+  return (
+    <>
+      <h1 className="text-5xl font-black">Mis Proyectos</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+    </>
+  );
 }
