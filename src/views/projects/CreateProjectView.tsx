776e9f3129d082fb24aad932b89ebaad4f990659
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-20 22:57:23 -0400

    instalando react-hook-form y zod para crear los campos y schemas del formulario

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,6 +1,25 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import ProjectForm from "@/components/projects/ProjectForm";
+import { ProjectFormData } from "types";
 
 export default function CreateProjectView() {
+  const initialValues: ProjectFormData = {
+    projectName: "",
+    clientName: "",
+    description: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const handleForm = (data: ProjectFormData) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="max-w-3xl mx-auto">
@@ -15,6 +34,16 @@ export default function CreateProjectView() {
             Volver a Proyectos
           </Link>
         </nav>
+
+        <form className="mt-10 bg-white shadow-lg p-10 rounded-lg" onSubmit={handleSubmit(handleForm)} noValidate>
+          <ProjectForm register={register} errors={errors} />
+
+          <input
+            type="submit"
+            value="Crear Proyecto"
+            className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+          />
+        </form>
       </div>
     </>
   );
