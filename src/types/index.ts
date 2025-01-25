f37cb0d9a2a6050805a7722f2a1b2e61e0837ca1
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-25 17:33:56 -0400

    mostando formulario en el modal

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,3 +24,18 @@ export const editProjectSchema = projectSchema.pick({
 
 export type Project = z.infer<typeof projectSchema>;
 export type ProjectFormData = Pick<Project, "clientName" | "projectName" | "description">;
+
+/** Tasks */
+export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"]);
+export type TaskStatus = z.infer<typeof taskStatusSchema>;
+
+export const taskSchema = z.object({
+  _id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  project: z.string(),
+  status: taskStatusSchema,
+});
+
+export type Task = z.infer<typeof taskSchema>;
+export type TaskFormData = Pick<Task, "name" | "description">;
