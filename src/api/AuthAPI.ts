fcc3f9c009d04dd5ac4d54f4462ff6fc560c46df
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-07 22:09:41 -0400

    creado metodo para forgot password

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,5 +1,11 @@
 import api from "@/lib/axios";
-import { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
+import {
+  ConfirmToken,
+  ForgotPasswordForm,
+  RequestConfirmationCodeForm,
+  UserLoginForm,
+  UserRegistrationForm,
+} from "../types";
 import { isAxiosError } from "axios";
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -43,6 +49,18 @@ export async function authenticateUser(formData: UserLoginForm) {
     const url = "/auth/login";
     const { data } = await api.post<string>(url, formData);
     localStorage.setItem("AUTH_TOKEN", data);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function forgotPassword(formData: ForgotPasswordForm) {
+  try {
+    const url = "/auth/forgot-password";
+    const { data } = await api.post<string>(url, formData);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
