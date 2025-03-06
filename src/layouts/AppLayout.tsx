39201f4f00a041ddc05fc303ee457209e438790c
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-03-06 09:14:33 -0400

    obteniendo el username y mostrando en la UI

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -12,29 +12,30 @@ export default function AppLayout() {
     return <Navigate to="/auth/login" />;
   }
 
-  return (
-    <>
-      <header className="bg-gray-800 py-5">
-        <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-64">
-            <Link to={"/"}>
-              <Logo />
-            </Link>
+  if (data)
+    return (
+      <>
+        <header className="bg-gray-800 py-5">
+          <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className="w-64">
+              <Link to={"/"}>
+                <Logo />
+              </Link>
+            </div>
+
+            <NavMenu name={data.name} />
           </div>
+        </header>
 
-          <NavMenu />
-        </div>
-      </header>
+        <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+          <Outlet />
+        </section>
 
-      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
-        <Outlet />
-      </section>
+        <footer className="py-5">
+          <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
+        </footer>
 
-      <footer className="py-5">
-        <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
-      </footer>
-
-      <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
-    </>
-  );
+        <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
+      </>
+    );
 }
