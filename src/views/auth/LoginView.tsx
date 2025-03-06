a795435ab1f9d2a6dac14e21d449448dce4bd793
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-03-06 12:11:58 -0400

    Redireccionando a la pagina principal despes de loguearse

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { UserLoginForm } from "@/types/index";
 import ErrorMessage from "@/components/ErrorMessage";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { authenticateUser } from "@/api/AuthAPI";
 import { toast } from "react-toastify";
@@ -16,6 +16,7 @@ export default function LoginView() {
     handleSubmit,
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
+  const navigate = useNavigate();
 
   const { mutate } = useMutation({
     mutationFn: authenticateUser,
@@ -23,7 +24,7 @@ export default function LoginView() {
       toast.error(error.message);
     },
     onSuccess: () => {
-      toast.success("Iniciando Sesion");
+      navigate("/");
     },
   });
 
